b2c06903b6494eb993db072c59378b2d25023311
Sergey Veremiychuk <veremey4uk@gmail.com>
2019-01-11 22:11:21 +0200

    design fixes

--- markup/components/header/data/data.js
+++ markup/components/header/data/data.js
@@ -1,42 +1,42 @@
 var data = {header: {
 	contact: {
 		item: [
-			{link: 'about'},
-			{link: 'games'},
-			{link: 'career'},
-			{link: 'contacts', active: 'is-active'}
+			{link: 'about-2', desc: 'about'},
+			{link: 'games-2', desc: 'games'},
+			{link: 'career-2', desc: 'career'},
+			{link: 'contacts-2', desc: 'contacts', active: 'is-active'}
 		]
 	},
 	career: {
 		item: [
-			{link: 'about'},
-			{link: 'games'},
-			{link: 'career', active: 'is-active'},
-			{link: 'contacts'}
+			{link: 'about-2', desc: 'about'},
+			{link: 'games-2', desc: 'games'},
+			{link: 'career-2', desc: 'career', active: 'is-active'},
+			{link: 'contacts-2', desc: 'contacts'}
 		]
 	},
 	about: {
 		item: [
-			{link: 'about', active: 'is-active'},
-			{link: 'games'},
-			{link: 'career'},
-			{link: 'contacts'}
+			{link: 'about-2', desc: 'about', active: 'is-active'},
+			{link: 'games-2', desc: 'games'},
+			{link: 'career-2', desc: 'career'},
+			{link: 'contacts-2', desc: 'contacts'}
 		]
 	},
 	main: {
 		item: [
-			{link: 'about'},
-			{link: 'games'},
-			{link: 'career'},
-			{link: 'contacts'}
+			{link: 'about-2', desc: 'about'},
+			{link: 'games-2', desc: 'games'},
+			{link: 'career-2', desc: 'career'},
+			{link: 'contacts-2', desc: 'contacts'}
 		]
 	},
 	games: {
 		item: [
-			{link: 'about'},
-			{link: 'games', active: 'is-active'},
-			{link: 'career'},
-			{link: 'contacts'}
+			{link: 'about-2', desc: 'about'},
+			{link: 'games-2', desc: 'games', active: 'is-active'},
+			{link: 'career-2', desc: 'career'},
+			{link: 'contacts-2', desc: 'contacts'}
 		]
 	}
 }};
